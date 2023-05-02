12b4736224f38bd8c6e85e912668127756c25a4b
Steve Firsake <firsake@gmail.com>
2023-05-02 11:21:13 +0300

    Delete sqldbSoma.py

--- sqldbSoma.py
+++
@@ -1,5 +0,0 @@
-import sqlalchemy
-import pandas as pd
-engine = sqlalchemy.create_engine('mysql+pymysql://root:74#falls@localhost/SpatiaInvSpc')
-df = pd.read_sql_table('fielddata', engine)
-print(df.head())
